10f20f04e8c9d4011860a5d6f6348629569edc86
Mio Łuczak <mioluczak@gmail.com>
2024-06-06 10:40:23 +0200

    Task 2; Converting JSON data to JS Object with `JSON.parse()` method

---
+++ script.js
@@ -0,0 +1,12 @@
+// === Task 2 ===
+
+// JSON data:
+const json = '{"name":"Mango","age":3,"isHappy":true}';
+
+// Parsing JSON data to create JS Object (called data)
+const data = JSON.parse(json);
+
+// Printing JS Object in console
+console.log(data);
+
+// Using Node.js to run script
